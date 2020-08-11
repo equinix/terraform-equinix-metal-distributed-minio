9fbfb7d946624d9429fbde71afa0be2883a14628
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-08-11 11:38:27 -0400

    Update user_data.sh

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -58,6 +58,7 @@ root_disk=`df -h | sort -k 6 | head -1 | cut -c1-8`
 # This is useful for software defined storage solutions where its best to use homogeneous drives
 # You can view drive models by running: lsblk -d -o name,size,model,rota
 # Example: DRIVE_MODEL="HGST HUS728T8TAL"
+# Specifying multiple drives is also an option when you are using the same server type with slightly revised drive models.
 # To specify multiple drive models to be used for Minio, the string should be in this format: "DRIVE_MODEL_1\|DRIVE_MODEL_2" where each model name is separated by \|
 # Example: DRIVE_MODEL="HGST_HUS728T8TAL\|Micron_5200_MTFD"
 # Leaving the string empty (DRIVE_MODEL="") will make the script use any drive model
