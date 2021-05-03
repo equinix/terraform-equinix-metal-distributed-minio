4795f74725e45c9ecdf4347d01d70bf70abb266c
Marques Johansson <mjohansson@equinix.com>
2021-05-03 19:23:11 -0400

    run terraform fmt
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "minio_endpoints" {
-    value = formatlist("%s minio endpoint is %s", metal_device.minio-distributed-cluster[*].hostname, data.template_file.endpoint[*].rendered)
+  value = formatlist("%s minio endpoint is %s", metal_device.minio-distributed-cluster[*].hostname, data.template_file.endpoint[*].rendered)
 }
 
-output "minio_access_key"{
-    value = random_string.minio_access_key.result
+output "minio_access_key" {
+  value = random_string.minio_access_key.result
 }
 
-output "minio_access_secret"{
-    value = random_string.minio_secret_key.result
+output "minio_access_secret" {
+  value = random_string.minio_secret_key.result
 }
 
 output "minio_region_name" {
-    value = "us-east-1"
+  value = "us-east-1"
 }
